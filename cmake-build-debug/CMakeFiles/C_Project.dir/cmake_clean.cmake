6726090e909e9723a171fb139f26ede3baf39848
home_xiaoyan <home@win.com>
2022-09-27 22:05:34 +0800

    day3

---
+++ cmake-build-debug/CMakeFiles/C_Project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Project.dir/初识C(3).c.obj"
+  "CMakeFiles/C_Project.dir/初识C(3).c.obj.d"
+  "C_Project.exe"
+  "C_Project.exe.manifest"
+  "C_Project.lib"
+  "C_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
